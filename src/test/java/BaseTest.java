9127b71ed138e68c3e735b2eb6c5eb615837e07d
ush24 <usha.kommu@testpro.io>
2023-06-28 22:36:57 -0700

    Add Homework17

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,11 +1,58 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
-public class BaseTest {
+import java.time.Duration;
 
+public class BaseTest {
+    public static WebDriver driver=null;
+    public static String url="https://qa.koel.app/";
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
+@BeforeMethod
+    public void launchBrowser(){
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver=new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
 }
+
+@AfterMethod
+    public void closeBrowser() {
+    driver.quit();
+}
+public static void navigateToPage() {
+    driver.get(url);
+}
+public static void provideEmail(String email) {
+    WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+    emailField.click();
+    emailField.clear();
+    emailField.sendKeys(email);
+}
+    public static void providePassword(String password) {
+        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+        passwordField.click();
+        passwordField.clear();
+        passwordField.sendKeys(password);
+    }
+    public static void clickSubmit() {
+        WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
+        submit.click();
+    }
+
+}
+
+
+
+
