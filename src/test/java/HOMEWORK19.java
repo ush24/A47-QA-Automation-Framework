b62dedcaf96902c6331408153f1c1c03a710d139
ush24 <usha.kommu@testpro.io>
2023-07-06 10:51:14 -0700

    Homework19

--- src/test/java/HOMEWORK19.java
+++ src/test/java/HOMEWORK19.java
@@ -12,20 +12,30 @@ public class HOMEWORK19 extends BaseTest {
         clickSubmit();
         openPlaylist();
         clickDeletePlaylistBtn();
+        Thread.sleep(3000);
+        clickonConfirmationOkBtn();
+        Thread.sleep(3000);
         Assert.assertTrue(getDeletedPlaylistMSg().contains(deletedPlaylistMsg));
     }
     public void openPlaylist() {
         WebElement emptyPlaylist = driver.findElement(By.cssSelector(".playlist:nth-child(3)"));
         emptyPlaylist.click();
     }
-    public void clickDeletePlaylistBtn() {
+    public void clickDeletePlaylistBtn() throws InterruptedException {
         WebElement deletePlaylist = driver.findElement(By.cssSelector(".btn-delete-playlist"));
         deletePlaylist.click();
+        Thread.sleep(3000);
     }
     public String getDeletedPlaylistMSg() {
+
         WebElement notificationMsg = driver.findElement(By.cssSelector("div.success.show"));
         return notificationMsg.getText();
     }
+   public void clickonConfirmationOkBtn() {
+        WebElement confirmationOkBtn = driver.findElement(By.cssSelector("div[class='dialog']>div>nav>button[class='ok']"));
+        confirmationOkBtn.click();
+    }
+
 }
 
 
